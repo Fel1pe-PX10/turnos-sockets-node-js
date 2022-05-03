c77b1e2af97c308d590d63ec1126ba0633fb606d
Felipe Paez <ofpaez87@gmail.com>
2022-05-03 18:54:18 -0500

    Primer Commit

---
+++ public/js/escritorio.js
@@ -0,0 +1,57 @@
+// Referencias HTMl
+const lblEscritorio = document.querySelector('h1');
+const btnAtender    = document.querySelector('button');
+const lblTicket     = document.querySelector('small');
+const divAlerta     = document.querySelector('.alert');
+
+const searchParams = new URLSearchParams( window.location.search );
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerHTML = escritorio; 
+
+divAlerta.style.display = 'none';
+
+
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    btnAtender.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnAtender.disabled = true;
+});
+
+
+socket.on('ultimo-ticket', (ultimo) => {
+   // console.log(ultimo+'adf');
+   // lblNuevoTicket.innerHTML = 'Ticket'+ultimo;
+});
+
+btnAtender.addEventListener( 'click', () => {
+
+    
+    socket.emit( 'atender-ticket', {escritorio}, ( {ticket, msg, ok} ) => {
+        //console.log(ticket)
+        if(!ok){
+            lblTicket.innerText = 'Nadie';
+            return divAlerta.style.display = '';
+        }
+
+        lblTicket.innerText = `Ticket ${ticket.numero}`;
+    
+    });
+
+});
---
+++ sockets/controller.js
@@ -0,0 +1,48 @@
+const TicketControl = require('../models/ticket-control');
+
+const ticketControl = new TicketControl;
+
+const socketController = (socket) => {
+
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual', ticketControl.ultimos4);
+    
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+
+    });
+
+    socket.on('atender-ticket', ({escritorio}, callback) => {
+        if(!escritorio){
+            return callback({
+                ok:false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+        if(!ticket){
+            return callback({
+                ok:false,
+                msg: 'No hay tickets pendientes'
+            });
+        }
+        else {
+            return callback({
+                ok: true,
+                ticket
+            });
+        }
+    });
+
+}
+
+
+
+module.exports = {
+    socketController
+}
+
